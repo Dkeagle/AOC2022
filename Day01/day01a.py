12e408614c8e53529d5af79124c872efcad5cefb
Dkeagle <taildeman_dylan@outlook.com>
2022-12-01 11:19:14 +0100

    current update day01

--- Day01/day01a.py
+++ Day01/day01a.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+total = 0
+
+with open('input.txt', 'r') as file:
+    for line in file:
+        if line == "\n" or not line:
+            print(total)
+            total = 0
+            continue
+        total += int(line)
