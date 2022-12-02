06a6e153742e27dbaa40cc937801cdba32ee68af
Dkeagle <taildeman_dylan@outlook.com>
2022-12-02 14:11:23 +0100

    Last fun for Day 02

--- Day02/day02b.php
+++ Day02/day02b.php
@@ -8,15 +8,15 @@
     while(!feof($file)){
         $line = trim(fgets($file));
         switch ($line) {
-            case 'B Z': $score += 1;
-            case 'A Z': $score += 1;
-            case 'C Z': $score += 1;
-            case 'C Y': $score += 1;
-            case 'B Y': $score += 1;
-            case 'A Y': $score += 1;
-            case 'A X': $score += 1;
-            case 'C X': $score += 1;
-            case 'B X': $score += 1; 
+            case 'B Z': $score++;
+            case 'A Z': $score++;
+            case 'C Z': $score++;
+            case 'C Y': $score++;
+            case 'B Y': $score++;
+            case 'A Y': $score++;
+            case 'A X': $score++;
+            case 'C X': $score++;
+            case 'B X': $score++; 
             default: break;
         }
     }
