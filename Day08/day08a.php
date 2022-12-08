1c04bf4d304a9690aea706fe3ef3264abbff2be3
Dkeagle <taildeman_dylan@outlook.com>
2022-12-08 22:51:44 +0100

    End of Day 08

---
+++ Day08/day08a.php
@@ -0,0 +1,64 @@
+<?php
+    $file = fopen("input.txt", "r");
+    $forest = [];
+    $visible_trees = 0;
+
+    // load the input file as a two dimensionnal array
+    $i = 0;
+    while(!feof($file)){
+        $line = str_split(trim(fgets($file)));
+        $forest[$i] = [];
+        $j = 0;
+        foreach($line as $char){
+            $forest[$i][$j] = $char;
+            $j++;
+        }
+        $i++;
+    }
+
+    // going through the forest to check every tree
+    $rows = count($forest);
+    $cols = count($forest[0]);
+    for($i = 1; $i < $rows - 1; $i++){
+        for($j = 1; $j < $cols - 1; $j++){
+            $tree = $forest[$i][$j];
+            $north = $south = $west = $east = 0;
+            // check north
+            for($k = 0; $k < $i; $k++){
+                if($tree <= $forest[$k][$j]){
+                    $north++;
+                }
+                if($north){ break; }
+            }
+            // check south
+            for($k = $i + 1; $k < $rows; $k++){
+                if($tree <= $forest[$k][$j]){
+                    $south++;
+                }
+                if($south){ break; }
+            }
+            // check west
+            for($k = 0; $k < $j; $k++){
+                if($tree <= $forest[$i][$k]){
+                    $west++;
+                }
+                if($west){ break; }
+            }
+            // check east
+            for($k = $j + 1; $k < $cols; $k++){
+                if($tree <= $forest[$i][$k]){
+                    $east++;
+                }
+                if($east){ break; }
+            }
+            // if flag is not raised, then tree is visible: increment the counter
+            if(!($north && $south && $west && $east)){ $visible_trees++; }
+        }
+    }
+
+    // add the border trees to the total, as they're visible from outside of the forest
+    $visible_trees += ($rows * 2) + ($cols * 2) - 4;
+
+    // display the result
+    echo $visible_trees."\n";
+?>
---
+++ Day08/day08b.php
@@ -0,0 +1,63 @@
+<?php
+    $file = fopen("input.txt", "r");
+    $forest = [];
+    $scenic_score = 0;
+
+    // load the input file as a two dimensionnal array
+    $i = 0;
+    while(!feof($file)){
+        $line = str_split(trim(fgets($file)));
+        $forest[$i] = [];
+        $j = 0;
+        foreach($line as $char){
+            $forest[$i][$j] = $char;
+            $j++;
+        }
+        $i++;
+    }
+
+    // going through the forest to check every tree
+    $rows = count($forest);
+    $cols = count($forest[0]);
+    for($i = 1; $i < $rows - 1; $i++){
+        for($j = 1; $j < $cols - 1; $j++){
+            $tree = $forest[$i][$j];
+            $north = $south = $west = $east = 1;
+            // count north
+            for($k = $i - 1; $k >= 0; $k--){
+                if($forest[$k][$j] >= $tree || $k == 0){
+                    $north = $i - $k;
+                    break;
+                }
+            }
+            // count south
+            for($k = $i + 1; $k < $rows; $k++){
+                if($forest[$k][$j] >= $tree || $k == $rows - 1){
+                    $south = $k - $i;
+                    break;
+                }
+            }
+            // count west
+            for($k = $j - 1; $k >= 0; $k--){
+                if($forest[$i][$k] >= $tree || $k == 0){
+                    $west = $j - $k;
+                    break;
+                }
+            }
+            // count east
+            for($k = $j + 1; $k < $cols; $k++){
+                if($forest[$i][$k] >= $tree || $k == $cols - 1){
+                    $east = $k - $j;
+                    break;
+                }
+            }
+            // calc tree score
+            $tmp = $north * $south * $west * $east;
+            // if tree score is bigger than last best, update
+            if($tmp > $scenic_score){ $scenic_score = $tmp; }
+        }
+    }
+
+    // display the result
+    echo $scenic_score."\n";
+?>
